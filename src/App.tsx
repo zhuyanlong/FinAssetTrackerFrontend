628643279e87705903644fd1b33b19332ca4d55b
AllenChu <zhuyanlongcs@hotmail.com>
2025-12-02 12:26:20 +0800

    fix bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import AssetTracker from './components/AssetTracker';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
       <header></header>
